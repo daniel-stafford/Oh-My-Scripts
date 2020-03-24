9b3e8e475c714c857eaa82d2d676a2e07af6111a
Daniel-Stafford <33048490+daniel-stafford@users.noreply.github.com>
2020-03-24 21:30:54 +0200

    Add comments, validate user argument length

--- count-commits.sh
+++ count-commits.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
 
+# script has basic functionality, though could still be improved.  Output could be improved when
+# user arguments are provided.  If the "count-commits.sh dan" is provided, output will be dan: number of commits,
+# instead of danie-stafford: number of commits.
+
+# allow me to loop though the git log via comma delinator
 OLDIFS=$IFS
 
 IFS=","
 
+#provide instructor when users adds -h parameter
 if [ "$1" == "-h" ]; then
-  echo "Usage: $(basename $0) If you run this script without arguments, output will be a list of author names along with their number of commits.  If names of authors are provided as arugments (write only first name), output will be only commits for those authors."
+  echo "Usage: $(basename $0) If you run this script without arguments, output will be a
+  list of author names along with their number of commits.  If names of authors are provided
+  as arugments (write at least 3 characters per user name, which are case insensitive) output will
+  be only commits from those authors."
   exit 0
 fi
 
+# get commit author from git log
 gitlog=$(git log --pretty="%an,")
 
+# if no arguments, loop through git log, sort/count commits per user, format output as user - # of commits
+# Note: that awk word reversal came from stack overrflow, working on understanding it...
 if [ "$#" -eq 0 ]; then
   for user in "${gitlog[@]}"; do
     echo "$user"
-  done | sort | uniq -c | sort -nr | awk '{s=$1;$1=$NF;$NF=s}1' | sed 's/,/ -/g'
+  done | uniq -c | sort -nr | awk '{s=$1;$1=$NF;$NF=s}1' | sed 's/,/ -/g'
 fi
 
+# function for looping with user arugments
 function loop_gitLog() {
   for user in "${gitlog[@]}"; do
-    echo $user
-  done | grep -c $1
+    echo "$user"
+  done | grep -ic $1
+  return 0
 }
 
+# if arugments are givne, loop through eahc user argument, invoke another loop that counts the number
+# of commits per given user
 for userArg in "$@"; do
-  result=$(loop_gitLog $userArg)
-  echo "$userArg - $result"
+  # requier arugments to be at least three characters
+  if [ "${#userArg}" -lt 3 ]; then
+    echo 'Please provide user names with at least 3 characters'
+    exit 1
+  else
+    result=$(loop_gitLog $userArg)
+    echo "$userArg - $result"
+  fi
 done
 
+# restore old IFS to avoid messing up my computer!
 IFS=$OLDIFS
 
 exit 0
