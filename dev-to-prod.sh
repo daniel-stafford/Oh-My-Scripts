5f71f25e19e03af74a48c80ff28bc728af193f75
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2020-08-03 16:36:10 +0300

    Fix typos

--- dev-to-prod.sh
+++ dev-to-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
-    echo "👎 $(tput setaf 1) Please provide the name of the dev dependecy you want to move to proudct dependcies"
+    echo "👎 $(tput setaf 1) Please provide the name of the dev dependency you want to move to proudct dependencies"
     echo "$(tput setaf 6) e.g. bash dev-to-prod.sh $(tput setaf 3) nodemon"
     exit 2
 fi
--- prod-to-dev.sh
+++ prod-to-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
-    echo "👎 $(tput setaf 1) Please provide the name of the product dependecy you want to move to dev dependcies"
+    echo "👎 $(tput setaf 1) Please provide the name of the product dependency you want to move to dev dependencies"
     echo "$(tput setaf 6) e.g. bash prod-to-dev.sh $(tput setaf 3) nodemon"
     exit 2
 fi
