b7242a28de4423eaff0d50c945b85556d7b04601
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-06-22 15:23:58 -0700

    Add duet script

---
+++ duet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+displayplacer "id:F466F621-B5FA-04A0-0800-CFA6C258DECD res:1440x900 color_depth:4 scaling:on origin:(0,0) degree:0" "id:00001130-0000-0000-0000-0000003F0041 res:1020x764 color_depth:4 scaling:off origin:(222,900) degree:0"
+exit 0
